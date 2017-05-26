da9924d22e02d858a76261a325c016f1b1abb09a
daniel <danielnilssoncole@gmail.com>
2017-05-26 07:06:11 -0400

    file objects

--- file_objects.py
+++ file_objects.py
@@ -41,10 +41,23 @@ with open('test.txt', 'r') as f:
 
 
 #copy from one file to another
-with open('test.txt', 'r') as rf:
-    with open('test_copy.txt', 'w') as wf:
-        for line in rf:
-            wf.write(line)
+# with open('test.txt', 'r') as rf:
+#     with open('test_copy.txt', 'w') as wf:
+#         for line in rf:
+#             wf.write(line)
 
 
 #working with image files: rb, wb
+#need to open images in binary mode: rb, wb
+# with open('earth.jpg', 'rb') as rf:
+#     with open('earth_copy.jpg', 'wb') as wf:
+#         for line in rf:
+#             wf.write(line)
+
+with open('earth.jpg', 'rb') as rf:
+    with open('earth_copy.jpg', 'wb') as wf:
+        chunk_size = 4096
+        rf_chunk = rf.read(chunk_size)
+        while len(rf_chunk) > 0:
+            wf.write(rf_chunk)
+            rf_chunk = rf.read(chunk_size)
